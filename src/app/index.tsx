1d9389c476dbfa6d8c909d9a2f0c7fce21393608
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2026-08-20 19:32:20 +0100

    Build: Created the base style of the home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>The Poker Table</Text>
+
+      <Pressable
+        onPress={() => {
+          console.log("Pressable Pressed");
+        }}
+        style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+      >
+        <Text style={styles.buttonText}>PLAY</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={() => {
+          console.log("Pressable Pressed");
+        }}
+        style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+      >
+        <Text style={styles.buttonText}>SETTINGS</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,7 +29,31 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "green",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "700",
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 180,
+    height: 52,
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  buttonPressed: {
+    backgroundColor: "lightgray",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
