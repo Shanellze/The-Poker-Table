3d4603d3f0abbd0c4eac12ac2461cddcd6e5fc8b
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2026-08-26 19:53:17 +0100

    Feat: Implement card display and hand dealing functionality

--- src/app/game.tsx
+++ src/app/game.tsx
@@ -1,8 +1,78 @@
 import { useRouter } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { createFreshDeck, dealHand, type Card } from "../utils/deck";
+
+const cardImageMap: Record<string, any> = {
+  "2-hearts": require("../../assets/images/cards/2_of_hearts.png"),
+  "3-hearts": require("../../assets/images/cards/3_of_hearts.png"),
+  "4-hearts": require("../../assets/images/cards/4_of_hearts.png"),
+  "5-hearts": require("../../assets/images/cards/5_of_hearts.png"),
+  "6-hearts": require("../../assets/images/cards/6_of_hearts.png"),
+  "7-hearts": require("../../assets/images/cards/7_of_hearts.png"),
+  "8-hearts": require("../../assets/images/cards/8_of_hearts.png"),
+  "9-hearts": require("../../assets/images/cards/9_of_hearts.png"),
+  "10-hearts": require("../../assets/images/cards/10_of_hearts.png"),
+  "J-hearts": require("../../assets/images/cards/jack_of_hearts.png"),
+  "Q-hearts": require("../../assets/images/cards/queen_of_hearts.png"),
+  "K-hearts": require("../../assets/images/cards/king_of_hearts.png"),
+  "A-hearts": require("../../assets/images/cards/ace_of_hearts.png"),
+  "2-diamonds": require("../../assets/images/cards/2_of_diamonds.png"),
+  "3-diamonds": require("../../assets/images/cards/3_of_diamonds.png"),
+  "4-diamonds": require("../../assets/images/cards/4_of_diamonds.png"),
+  "5-diamonds": require("../../assets/images/cards/5_of_diamonds.png"),
+  "6-diamonds": require("../../assets/images/cards/6_of_diamonds.png"),
+  "7-diamonds": require("../../assets/images/cards/7_of_diamonds.png"),
+  "8-diamonds": require("../../assets/images/cards/8_of_diamonds.png"),
+  "9-diamonds": require("../../assets/images/cards/9_of_diamonds.png"),
+  "10-diamonds": require("../../assets/images/cards/10_of_diamonds.png"),
+  "J-diamonds": require("../../assets/images/cards/jack_of_diamonds.png"),
+  "Q-diamonds": require("../../assets/images/cards/queen_of_diamonds.png"),
+  "K-diamonds": require("../../assets/images/cards/king_of_diamonds.png"),
+  "A-diamonds": require("../../assets/images/cards/ace_of_diamonds.png"),
+  "2-clubs": require("../../assets/images/cards/2_of_clubs.png"),
+  "3-clubs": require("../../assets/images/cards/3_of_clubs.png"),
+  "4-clubs": require("../../assets/images/cards/4_of_clubs.png"),
+  "5-clubs": require("../../assets/images/cards/5_of_clubs.png"),
+  "6-clubs": require("../../assets/images/cards/6_of_clubs.png"),
+  "7-clubs": require("../../assets/images/cards/7_of_clubs.png"),
+  "8-clubs": require("../../assets/images/cards/8_of_clubs.png"),
+  "9-clubs": require("../../assets/images/cards/9_of_clubs.png"),
+  "10-clubs": require("../../assets/images/cards/10_of_clubs.png"),
+  "J-clubs": require("../../assets/images/cards/jack_of_clubs.png"),
+  "Q-clubs": require("../../assets/images/cards/queen_of_clubs.png"),
+  "K-clubs": require("../../assets/images/cards/king_of_clubs.png"),
+  "A-clubs": require("../../assets/images/cards/ace_of_clubs.png"),
+  "2-spades": require("../../assets/images/cards/2_of_spades.png"),
+  "3-spades": require("../../assets/images/cards/3_of_spades.png"),
+  "4-spades": require("../../assets/images/cards/4_of_spades.png"),
+  "5-spades": require("../../assets/images/cards/5_of_spades.png"),
+  "6-spades": require("../../assets/images/cards/6_of_spades.png"),
+  "7-spades": require("../../assets/images/cards/7_of_spades.png"),
+  "8-spades": require("../../assets/images/cards/8_of_spades.png"),
+  "9-spades": require("../../assets/images/cards/9_of_spades.png"),
+  "10-spades": require("../../assets/images/cards/10_of_spades.png"),
+  "J-spades": require("../../assets/images/cards/jack_of_spades.png"),
+  "Q-spades": require("../../assets/images/cards/queen_of_spades.png"),
+  "K-spades": require("../../assets/images/cards/king_of_spades.png"),
+  "A-spades": require("../../assets/images/cards/ace_of_spades.png"),
+  "red-joker": require("../../assets/images/cards/red_joker.png"),
+  "black-joker": require("../../assets/images/cards/black_joker.png"),
+};
+
+function getCardImageSource(card: Card) {
+  return cardImageMap[card.id] ?? cardImageMap["red-joker"];
+}
 
 export default function Game() {
   const router = useRouter();
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    const { shuffledDeck } = createFreshDeck();
+    const { hand } = dealHand(shuffledDeck, 5);
+    setCards(hand);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -87,31 +157,15 @@ export default function Game() {
 
       {/* Displays the active 5-card hand */}
       <View style={styles.cardContainer}>
-        <View style={styles.card}>
-          <Text>Card 1</Text>
-          <Text>10</Text>
-          <Text>spades</Text>
-        </View>
-        <View style={styles.card}>
-          <Text>Card 2</Text>
-          <Text>9</Text>
-          <Text>hearts</Text>
-        </View>
-        <View style={styles.card}>
-          <Text>Card 3</Text>
-          <Text>5</Text>
-          <Text>diamond</Text>
-        </View>
-        <View style={styles.card}>
-          <Text>Card 4</Text>
-          <Text>A</Text>
-          <Text>clover</Text>
-        </View>
-        <View style={styles.card}>
-          <Text>Card 5</Text>
-          <Text>Q</Text>
-          <Text>hearts</Text>
-        </View>
+        {cards.map((card, index) => (
+          <View key={card.id} style={styles.card}>
+            <Image
+              source={getCardImageSource(card)}
+              style={styles.cardImage}
+              resizeMode="contain"
+            />
+          </View>
+        ))}
       </View>
 
       {/* Displays the player's financial information */}
@@ -224,11 +278,14 @@ const styles = StyleSheet.create({
   },
   card: {
     flex: 1,
-    marginHorizontal: 12,
-    alignItems: "center",
-    justifyContent: "center",
+    marginHorizontal: 6,
     borderRadius: 10,
-    backgroundColor: "white",
+    overflow: "hidden",
+  },
+  cardImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
   },
   betContainer: {
     flexDirection: "row",
