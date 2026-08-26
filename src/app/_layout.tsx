241d7b4b7d58ceccb2af02a45fd646683b750477
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2026-08-26 04:50:50 +0100

    Feat: Implement audio player for main theme music

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,19 @@
+import { useAudioPlayer } from "expo-audio";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const player = useAudioPlayer(require("../../assets/audio/mainTheme.mp3"));
+
+  useEffect(() => {
+    player.loop = true;
+    player.play();
+
+    return () => {
+      player.pause();
+    };
+  }, [player]);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
